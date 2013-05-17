016dd9ac93fe22595573e17a51452600ae1f7bdd
Kerem Gökhan <mail@keremgokhan.net>
2013-05-17 02:46:46 -0500

    uygulama hakkında, inbox vs. eklendi.

--- Kan_Ariyorum/scripts/hello-world.js
+++ Kan_Ariyorum/scripts/hello-world.js
@@ -13,26 +13,7 @@ function getLocation() {
     navigator.geolocation.getCurrentPosition(onGeolocationSuccess, onGeolocationError);
 }
 
-//=======================Say Hello (Page 1) Operations=======================//
-function sayHello() {
-    var sayHelloInputElem = document.getElementById('helloWorldInput');
-    var sayHelloTextElem = document.getElementById('helloWorldText');
-    var inputText = document.getElementById('txtName');
 
-    sayHelloTextElem.innerHTML = 'Hello, ' + inputText.value + '!';
-    sayHelloTextElem.style.display = 'block';
-    sayHelloInputElem.style.display = 'none';
-}
-
-function sayHelloReset() {
-    var sayHelloInputElem = document.getElementById('helloWorldInput');
-    var sayHelloTextElem = document.getElementById('helloWorldText');
-    var inputText = document.getElementById('txtName');
-
-    inputText.value = '';
-    sayHelloTextElem.style.display = 'none';
-    sayHelloInputElem.style.display = 'block';
-}
 
 //=======================Geolocation Operations=======================//
 // onGeolocationSuccess Geolocation
